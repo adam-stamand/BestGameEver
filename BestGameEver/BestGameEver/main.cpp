f2573d4d1a5a44459c256a004fcba17ab4b69283
Adam St. Amand <adam.stamand@gmail.com>
2017-10-21 17:25:13 -0700

    added mass

--- BestGameEver/BestGameEver/main.cpp
+++ BestGameEver/BestGameEver/main.cpp
@@ -1,16 +1,16 @@
 #include <SFML/Graphics.hpp>
+#include <string> 
 
 using namespace sf;
 
-
-
-#define VEL_MAX 1
-#define FRICTION 0.0005 // must be less than 1... or else you get like negative friction
-#define ACCEL	0.0001
-#define GRAVITY 0.00007
-#define JUMP	0.15
-#define JET_MAX	20000
-#define JET_ACCEL 0.00009
+#define VEL_MAX		.1
+#define FRICTION	0.0005  // must be less than 1... or else you get like negative friction
+#define FORCE		0.0001
+#define GRAVITY		0.0001
+#define JUMP_FORCE	0.25
+#define JET_MAX		100000
+#define JET_FORCE	0.00025
+#define MASS		1.4
 
 int main()
 {
@@ -21,11 +21,14 @@ int main()
 	font.loadFromFile("C:/Windows/Fonts/Arial.ttf");
 	sf::Clock deltaClock;
 	Text text;
+	Text jet_status;
+	std::string jetstring;
 	text.setFont(font);
-
+	jet_status.setFont(font);
 	char mander = '>';
+	bool jet_flag = false;
 
-	double x_pos=200.0, y_pos=200.0, x_vel=0, y_vel=0, jetpack = JET_MAX, jet_intr = 2;
+	double x_pos=200.0, y_pos=200.0, x_vel=0, y_vel=0, jetpack = JET_MAX;
 
 	while (window.isOpen())
 	{
@@ -62,81 +65,83 @@ int main()
 
 		}
 
-		// Manage the velocity of the player
+		//------------------------
+		//Player Movement
+		//------------------------
 
-		// Accelerate the player
+		// Adjust Velocity: Accelerate the player
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-			x_vel -= ACCEL;	// if key pressed, increase velocity
+			x_vel -= FORCE/MASS;	// if key pressed, increase velocity
 			x_vel = abs(x_vel) > VEL_MAX ? -1*VEL_MAX : x_vel; // cap velocity at VEL_MAX
 			mander = '<'; // change character direction
 		}
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-			y_vel -= ACCEL;
+			y_vel -= FORCE/MASS;
 			y_vel = abs(y_vel) > VEL_MAX ? -1 * VEL_MAX : y_vel;
 			mander = '^';
 		}
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-			x_vel += ACCEL;
+			x_vel += FORCE/MASS;
 			x_vel = abs(x_vel) > VEL_MAX ? VEL_MAX : x_vel;
 			mander = '>';
 		}
 
+		// Allows player to use jetpack
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && jetpack > 0) {
-			y_vel += JET_ACCEL;
-			y_vel = abs(y_vel) > VEL_MAX ? VEL_MAX : y_vel;
-			jetpack -= jet_intr;
+			y_vel += JET_FORCE/MASS;
+			jetpack--;
+			mander = 'v';
+			jet_flag = true;
+		}
+
+		// Allows player to jump
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && y_pos == 0) {
+			y_vel += JUMP_FORCE/MASS;
 			mander = 'v';
 		}
 
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && y_pos <= 0) {
-			y_vel += JUMP;
-			y_vel = abs(y_vel) > VEL_MAX ? VEL_MAX : y_vel;
-			mander = 'v';
-		}
-
-		// Deccelerate the player
-		if (x_vel < 0) {	// if player moving to the left
-			x_vel -= x_vel*FRICTION;	// reduce left velocity. FRICTION is < 1
-		}
-		else if (x_vel > 0){
-			x_vel -= x_vel*FRICTION;
-		}
-
-		if (y_vel < 0) {
-			y_vel -= y_vel*FRICTION;
-		}
-		else if (y_vel > 0) {
-			y_vel -= y_vel*FRICTION;
-		}
-		
-		// If above the "ground", apply gravity to velocity
-		if (y_pos > 0) {
-			y_vel -= GRAVITY;
-		}
+		//------------------------
+		//Player Environment
+		//------------------------
 
 
-		if (y_pos <= 0 && jetpack < JET_MAX) {
-			jetpack += jet_intr;
+		// Recharge Jetpack: Only if jet not used and jetpack isn't fully charged
+		if (jetpack < JET_MAX && jet_flag == false) {
+			jetpack += 2;
 		}
 
-		// Change position using velocity
+		// Adjust Velocity: Deccelerate the player
+		x_vel -= x_vel*FRICTION;	// reduce left velocity. FRICTION is < 1
+		y_vel -= (y_vel*FRICTION + GRAVITY*MASS); // Apply friction and gravity in Y direction
+
+		// Adjust Position: Change position using velocity
 		x_pos += x_vel;
 		y_pos += y_vel;
 
-
-		// Prevent player from floating off screen
-		if (y_pos < 0) {
+		// Edge Case: Prevent player from floating off screen; Reset jet used flag
+		if (y_pos <= 0) {
 			y_pos = 0;
+			y_vel = 0;
+			jet_flag = false;
 		}
 
+		//------------------------
+		//Windows managment
+		//------------------------
+
+		
 		// Print character to screen 
+		jetstring = std::to_string((int)jetpack);
 		text.setPosition(x_pos, y_pos);
 		text.setString(mander);
+		jet_status.setPosition(10, 500);
+		jet_status.setString(jetstring);
 		window.clear();
 		window.draw(text);
+		window.draw(jet_status);
 		window.display();
 
 	}
