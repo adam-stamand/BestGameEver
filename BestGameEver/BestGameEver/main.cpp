eb75d63089dd11b55c4a0a3dcffba4cbf956d032
Adam St. Amand <adam.stamand@gmail.com>
2017-10-22 05:41:03 -0700

    implemented movement class, added collisions and boundaries

--- BestGameEver/BestGameEver/main.cpp
+++ BestGameEver/BestGameEver/main.cpp
@@ -6,11 +6,168 @@ using namespace sf;
 #define VEL_MAX		.1
 #define FRICTION	0.0005  // must be less than 1... or else you get like negative friction
 #define FORCE		0.0001
-#define GRAVITY		0.0001
+#define GRAVITY		0.00008
 #define JUMP_FORCE	0.25
 #define JET_MAX		100000
 #define JET_FORCE	0.00025
 #define MASS		1.4
+
+
+
+
+class Movement {
+public:
+	Movement(float mass, float max_vel);
+	~Movement();
+
+	enum Direction {
+		UP,
+		DOWN,
+		LEFT,
+		RIGHT
+	};
+
+	void Push(Direction dir, float force);
+	void UpdatePosition();
+	float GetXPos();
+	float GetYPos();
+	float GetXVel();
+	float GetYVel();
+	void SetXPos(float x);
+	void SetYPos(float y);
+	void SetXVel(float x);
+	void SetYVel(float y);
+	float mass;
+	float max_vel;
+
+private:
+	void ApplyFriction();
+	void ApplyGravity();
+
+	float y_vel;
+	float x_vel;
+	float x_pos;
+	float y_pos;
+};
+
+
+Movement::Movement(float mass, float max_vel) {
+	this->mass = mass;
+	this->max_vel = max_vel;
+}
+
+Movement::~Movement() {
+}
+
+void Movement::SetXPos(float x) {
+	this->x_pos = x;
+}
+
+
+void Movement::SetYPos(float y) {
+	this->y_pos = y;
+}
+
+
+float Movement::GetXPos() {
+	return this->x_pos;
+}
+
+float Movement::GetYPos() {
+	return this->y_pos;
+}
+
+void Movement::SetYVel(float y) {
+	this->y_vel = y;
+}
+
+void Movement::SetXVel(float x) {
+	this->x_vel = x;
+}
+
+float Movement::GetXVel() {
+	return this->x_vel;
+}
+
+
+float Movement::GetYVel() {
+	return this->y_vel;
+}
+
+void Movement::Push(Direction dir, float force) {
+
+	switch (dir) {
+	case UP:
+		this->y_vel += force / this->mass;
+		break;
+
+	case DOWN:
+		this->y_vel -= force / this->mass;
+		break;
+
+	case LEFT:
+		this->x_vel -= force / this->mass;
+		break;
+
+	case RIGHT:
+		this->x_vel += force / this->mass;
+		break;
+	}
+}
+
+void Movement::ApplyFriction() {
+	this->y_vel -= this->y_vel * FRICTION;
+	this->x_vel -= this->x_vel * FRICTION;
+}
+
+
+void Movement::ApplyGravity() {
+	this->y_vel -= GRAVITY * this->mass; // mathematically incorrect
+}
+
+void Movement::UpdatePosition() {
+	this->ApplyGravity();
+	this->ApplyFriction();
+	this->x_pos += this->x_vel;
+	this->y_pos += this->y_vel;
+}
+
+
+template <typename T1, typename T2>
+void ObjectCollision(T1 &object1, T2 &object2) {
+
+	{
+		object1.SetXVel(
+			(((object1.mass - object2.mass) / (object1.mass + object2.mass)) * object1.GetXVel()) +
+			(((2 * object2.mass) / (object1.mass + object2.mass)) * object2.GetXVel())
+		);
+
+		object2.SetXVel(
+			(((object2.mass - object1.mass) / (object1.mass + object2.mass)) * object2.GetXVel()) +
+			(((2 * object1.mass) / (object1.mass + object2.mass)) * object1.GetXVel())
+		);
+
+		object1.SetYVel(
+			(((object1.mass - object2.mass) / (object1.mass + object2.mass)) * object1.GetYVel()) +
+			(((2 * object2.mass) / (object1.mass + object2.mass)) * object2.GetYVel())
+		);
+
+		object2.SetYVel(
+			(((object2.mass - object1.mass) / (object1.mass + object2.mass)) * object2.GetYVel()) +
+			(((2 * object1.mass) / (object1.mass + object2.mass)) * object1.GetYVel())
+		);
+	}
+}
+
+template <typename T1>
+void BoundaryXCollision(T1 &object) {
+	object.SetXVel(-object.GetXVel() * 0.8);
+}
+template <typename T1>
+void BoundaryYCollision(T1 &object) {
+	object.SetYVel(-object.GetYVel() * 0.8);
+}
+
 
 int main()
 {
@@ -21,14 +178,23 @@ int main()
 	font.loadFromFile("C:/Windows/Fonts/Arial.ttf");
 	sf::Clock deltaClock;
 	Text text;
+	Text box_text;
 	Text jet_status;
 	std::string jetstring;
+	box_text.setFont(font);
 	text.setFont(font);
 	jet_status.setFont(font);
 	char mander = '>';
+	char box = 'O';
 	bool jet_flag = false;
 
-	double x_pos=200.0, y_pos=200.0, x_vel=0, y_vel=0, jetpack = JET_MAX;
+	float jetpack = JET_MAX;
+	Movement box_mover(.25, 10);
+	box_mover.SetYPos(0);
+	box_mover.SetXPos(200);
+	Movement mover(MASS, VEL_MAX);
+	mover.SetYPos(0);
+	mover.SetXPos(0);
 
 	while (window.isOpen())
 	{
@@ -71,34 +237,31 @@ int main()
 
 		// Adjust Velocity: Accelerate the player
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-			x_vel -= FORCE/MASS;	// if key pressed, increase velocity
-			x_vel = abs(x_vel) > VEL_MAX ? -1*VEL_MAX : x_vel; // cap velocity at VEL_MAX
+			mover.Push(Movement::Direction::LEFT, FORCE);
 			mander = '<'; // change character direction
 		}
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-			y_vel -= FORCE/MASS;
-			y_vel = abs(y_vel) > VEL_MAX ? -1 * VEL_MAX : y_vel;
+			mover.Push(Movement::Direction::DOWN, FORCE);
 			mander = '^';
 		}
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-			x_vel += FORCE/MASS;
-			x_vel = abs(x_vel) > VEL_MAX ? VEL_MAX : x_vel;
+			mover.Push(Movement::Direction::RIGHT, FORCE);
 			mander = '>';
 		}
 
 		// Allows player to use jetpack
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && jetpack > 0) {
-			y_vel += JET_FORCE/MASS;
+			mover.Push(Movement::Direction::UP, JET_FORCE);
 			jetpack--;
 			mander = 'v';
 			jet_flag = true;
 		}
 
 		// Allows player to jump
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && y_pos == 0) {
-			y_vel += JUMP_FORCE/MASS;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && mover.GetYPos() <= 0) {
+			mover.Push(Movement::Direction::UP, JUMP_FORCE);
 			mander = 'v';
 		}
 
@@ -108,41 +271,82 @@ int main()
 		//------------------------
 
 
+		int temp_x_pos = mover.GetXPos() - box_mover.GetXPos();
+		int temp_y_pos = mover.GetYPos() - box_mover.GetYPos();
+		if (abs(temp_x_pos) < 20 && abs(temp_y_pos) < 20) {
+			ObjectCollision(mover, box_mover);
+		}
+
+
+		
+		if ((box_mover.GetXPos() < 0 && box_mover.GetXVel() < 0) || (box_mover.GetXPos() > 780 && box_mover.GetXVel() > 0)) {
+			BoundaryXCollision(box_mover);
+		}
+
+		if ((mover.GetXPos() <= 0 && mover.GetXVel() < 0) || (mover.GetXPos() >= 780 && mover.GetXVel() > 0)) {
+			BoundaryXCollision(mover);
+		}
+
+		if ((mover.GetYPos() <= 0 && mover.GetYVel() < 0) || (mover.GetYPos() >= 580 && mover.GetYVel() > 0)) {
+			BoundaryYCollision(mover);
+		}
+
+		if ((box_mover.GetYPos() <= 0 && box_mover.GetYVel() < 0) || (box_mover.GetYPos() >= 580 && box_mover.GetYVel() > 0)) {
+			BoundaryYCollision(box_mover);
+		}
+
 		// Recharge Jetpack: Only if jet not used and jetpack isn't fully charged
 		if (jetpack < JET_MAX && jet_flag == false) {
 			jetpack += 2;
 		}
 
-		// Adjust Velocity: Deccelerate the player
-		x_vel -= x_vel*FRICTION;	// reduce left velocity. FRICTION is < 1
-		y_vel -= (y_vel*FRICTION + GRAVITY*MASS); // Apply friction and gravity in Y direction
-
-		// Adjust Position: Change position using velocity
-		x_pos += x_vel;
-		y_pos += y_vel;
-
-		// Edge Case: Prevent player from floating off screen; Reset jet used flag
-		if (y_pos <= 0) {
-			y_pos = 0;
-			y_vel = 0;
-			jet_flag = false;
-		}
 
 		//------------------------
 		//Windows managment
 		//------------------------
 
-		
+		mover.UpdatePosition();
+		box_mover.UpdatePosition();
+
+
+
 		// Print character to screen 
 		jetstring = std::to_string((int)jetpack);
-		text.setPosition(x_pos, y_pos);
 		text.setString(mander);
+
+		if (abs(temp_x_pos) < 20 && abs(temp_y_pos) < 20) {
+			if (temp_x_pos <= 0) {
+				box_mover.SetXPos(box_mover.GetXPos() + 20 - abs(temp_x_pos));
+			}
+			else {
+				box_mover.SetXPos(box_mover.GetXPos() - (20 - abs(temp_x_pos)));
+			}
+		}
+
+		text.setPosition(mover.GetXPos(), mover.GetYPos());
+		box_text.setPosition(box_mover.GetXPos(), box_mover.GetYPos());
+
+		box_text.setString(box);
 		jet_status.setPosition(10, 500);
 		jet_status.setString(jetstring);
+
 		window.clear();
 		window.draw(text);
+		window.draw(box_text);
 		window.draw(jet_status);
 		window.display();
+
+		// Edge Case: Prevent player from floating off screen; Reset jet used flag
+		
+		if (mover.GetYPos() <= 0) {
+			mover.SetYPos(0);
+			jet_flag = false;
+		}
+
+		if (box_mover.GetYPos() <= 0) {
+			box_mover.SetYPos(0);
+		}
+
 
 	}
 
